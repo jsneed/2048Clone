2e7867fbdd5ae7e33177551c84ea5f9da019f115
Jeanne Sneed <jsneed@gmail.com>
2014-08-25 21:53:11 -0700

    First Commit

---
+++ 2048Clone/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  2048Clone
+//
+//  Created by Jeanne on 8/12/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ 2048Clone/GameScene.m
@@ -0,0 +1,13 @@
+//
+//  GameScene.m
+//  2048Clone
+//
+//  Created by Jeanne on 8/12/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import "GameScene.h"
+
+@implementation GameScene
+
+@end
